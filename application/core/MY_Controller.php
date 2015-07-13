230a1d640b04adafd3066b593139a1f3d89463e6
han <897475686@qq.com>
2015-07-13 08:13:58 +0800

    7-13

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,6 +1,5 @@
 <?php
-class MY_Controller extends CI_Controller
-{
+class MY_Controller extends CI_Controller{
 	public function __construct()
 	{
 		parent::__construct();
---
+++ application/service/auth_service.php
@@ -0,0 +1,7 @@
+<?php 
+class Auth_service extends MY_Service{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
