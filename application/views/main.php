d2158f3a50aa02588eed3519ddfe2653b1f24635
JiaZhuo Chen <cjzishades@gmail.com>
2015-07-17 15:29:59 +0800

    7-17

--- application/views/main.php
+++ application/views/main.php
@@ -1,201 +1,169 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-        <meta name="apple-mobile-web-app-capable" content="yes" />    
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="format-detection" content="telephone=yes"/>
-        <meta name="msapplication-tap-highlight" content="no" />
-        <script type="text/javascript" src="js/j162.min.js"></script>
-        <link href="css/common.css" type="text/css" rel="stylesheet">
-        <link href="css/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
-        <div id="vc_sidebar" class="sidebar">
-            <div class="name">
-                <div class="head">
-                    <a href="#">
-                        <img src="img/mm1.jpg">
-                    </a>
-                </div>
-                <div class="text">
-                    <a href="#">
-                        YOUNGCY
-                    </a>
-                    <div class="identity">
-                        <span class="icon identity"></span>
-                    </div>
-                </div>
-            </div>
-            
-			<div class="search">
-                   <form>
-                       <input id="" name="" type="text">
-                   </form>
-                    <div><a href="#"><i class="fa fa-search"></i></a></div>
-            </div>
-            
-            <div class="menu">
-                <ul>
-                    <li class="menu-list">
-                        <div class="menu-list-item">
-                            <a href="dfdfdf">
-                                <div class="icon">
-                                    <div class="home"></div>
-                                </div>
-                                <span class="menu-list-item-text">个人首页</span>
-                            </a>
-                        </div>
-                    </li>
-                    <a href="asasdas">
-                    <li class="menu-list active">
-                        <div class="menu-list-item">
-                            
-                                <div class="icon">
-                                    <div class="guanzhu"></div>
-                                </div>
-                                <span class="menu-list-item-text">我的关注</span>
-                            
-                        </div>
-                    </li>
-                    </a>
-                    <li class="menu-list">
-                        <div class="menu-list-item">
-                            <a href="#">
-                                <div class="icon">
-                                    <div class="tongji"></div>
-                                </div>
-                                <span class="menu-list-item-text">统计</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="menu-list">
-                        <div class="menu-list-item">
-                            <a href="#">
-                                <div class="icon">
-                                    <div class="sixin"></div>
-                                </div>
-                                <span class="menu-list-item-text">私信</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="menu-list">
-                        <div class="menu-list-item">
-                            <a href="#">
-                                <div class="icon">
-                                    <div class="setting"></div>
-                                </div>
-                                <span class="menu-list-item-text">设置</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="menu-list">
-                        <div class="menu-list-item">
-                            <a href="#">
-                                <div class="icon">
-                                    <div class="logout"></div>
-                                </div>
-                                <span class="menu-list-item-text">退出</span>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div id="vi_container" class="container">
-            <div id="shade"></div>
-            <div id="sbtn" class="sbtn">
-                <div class="icon">
-                    <div class="sidebtn"></div>
-                </div>
-            </div>
-            <div id="vi_content" class="content">
-<!--                <div id="vi_user" class="vi-user float-r"><a href="/">YOUNGCY</a></div>-->
-                <div id="vi_sign" class="vi-sign ">
-                    
-                    <div class="float-r">
-                     <a href="#" style="font-size:130%;">登陆</a>
-                    <i style="color:#FCEE21;font-weight: bold;padding:0 5px">/</i>
-                    <a href="#">注册</a>   
-                        
-                    </div>
-                    
-                </div>
-                <div id="vc_logo" class="logo">
-                    <div class="icon">
-                        <div class="logo-a"></div>
-                    </div>
-                </div>
-                <div id="vi_menu" class="vi-menu width-100p">
-                    <ul>
-                        <li><a href="#">发现</a></li>
-                        <li class="active"><a href="#">动态</a> </li>
-                        <li><a href="#">作品</a></li>
-                        <li><a href="#">文章</a></li>
-                    </ul>
-                </div>
-                <div id="vi_main" class="width-100p ">
-                    <div class="main width-100p">
-                        <ul>
-                            <li>
-                                <div class="activity">
-                                    <div class="activity-box"></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="activity">
-                                    <div class="activity-box"></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="activity">
-                                    <div class="activity-box"></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="activity">
-                                    <div class="activity-box"></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="activity">
-                                    <div class="activity-box"></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="activity">
-                                    <div class="activity-box"></div>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="loadmore width-100p">
-                            <a href="#" id="loadmore" class="btn load_btn">
-                                加载更多
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div id="vi_footer" class="width-100p">
-                    <div class="vi_footer">
-                        <div class="vi_footer_left">
-                            <div>
-                               &copy;&nbsp;artvc.cc&nbsp;京ICP备09025489号-4&nbsp; <a href="#">用户协议</a>&nbsp;-&nbsp;<a href="#">隐私政策</a>&nbsp;-&nbsp;<a href="#">联系我们</a>&nbsp;-&nbsp;<a href="#">意见反馈</a>
-                            </div>
-                            <div class="float-l">
-                                <a href="#"><i class="fa fa-weibo fa-2x icon"></i></a>
-                                <a href="#"><i class="fa fa-wechat fa-2x icon"></i></a>
-                                <a href="#"><i class="fa fa-facebook-official fa-2x icon"></i></a>
-                            </div>
-                            
-                        </div>
-                        <div class="vi_footer_right">
-                            <img src="img/QRCode.png">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>
-    <script type="text/javascript" src="js/vchome.js"></script>
+ <head> 
+  <meta charset="utf-8" /> 
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" /> 
+  <meta name="apple-mobile-web-app-capable" content="yes" /> 
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> 
+  <meta name="format-detection" content="telephone=yes" /> 
+  <meta name="msapplication-tap-highlight" content="no" /> 
+  <script type="text/javascript" src="js/j162.min.js"></script> 
+  <link href="css/common.css" type="text/css" rel="stylesheet" /> 
+  <link href="css/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet" /> 
+ </head> 
+ <body> 
+  <div id="vc_sidebar" class="sidebar"> 
+   <div class="name"> 
+    <div class="head"> 
+     <a href="#"> <img src="img/mm1.jpg" /> </a> 
+    </div> 
+    <div class="text"> 
+     <a href="#"> YOUNGCY </a> 
+     <div class="identity"> 
+      <span class="icon identity"></span> 
+     </div> 
+    </div> 
+   </div> 
+   <div class="search"> 
+    <form> 
+     <input id="" name="" type="text" /> 
+    </form> 
+    <div>
+     <a href="#"><i class="fa fa-search"></i></a>
+    </div> 
+   </div> 
+   <div class="menu"> 
+    <ul> 
+     <li class="menu-list"> 
+      <div class="menu-list-item"> 
+       <a href="dfdfdf"> 
+        <div class="icon"> 
+         <div class="home"></div> 
+        </div> <span class="menu-list-item-text">个人首页</span> </a> 
+      </div> </li> 
+     <a href="asasdas"> <li class="menu-list active"> 
+       <div class="menu-list-item"> 
+        <div class="icon"> 
+         <div class="guanzhu"></div> 
+        </div> 
+        <span class="menu-list-item-text">我的关注</span> 
+       </div> </li> </a> 
+     <li class="menu-list"> 
+      <div class="menu-list-item"> 
+       <a href="#"> 
+        <div class="icon"> 
+         <div class="tongji"></div> 
+        </div> <span class="menu-list-item-text">统计</span> </a> 
+      </div> </li> 
+     <li class="menu-list"> 
+      <div class="menu-list-item"> 
+       <a href="#"> 
+        <div class="icon"> 
+         <div class="sixin"></div> 
+        </div> <span class="menu-list-item-text">私信</span> </a> 
+      </div> </li> 
+     <li class="menu-list"> 
+      <div class="menu-list-item"> 
+       <a href="#"> 
+        <div class="icon"> 
+         <div class="setting"></div> 
+        </div> <span class="menu-list-item-text">设置</span> </a> 
+      </div> </li> 
+     <li class="menu-list"> 
+      <div class="menu-list-item"> 
+       <a href="#"> 
+        <div class="icon"> 
+         <div class="logout"></div> 
+        </div> <span class="menu-list-item-text">退出</span> </a> 
+      </div> </li> 
+    </ul> 
+   </div> 
+  </div> 
+  <div id="vi_container" class="container"> 
+   <div id="shade"></div> 
+   <div id="sbtn" class="sbtn"> 
+    <div class="icon"> 
+     <div class="sidebtn"></div> 
+    </div> 
+   </div> 
+   <div id="vi_content" class="content"> 
+    <!--                <div id="vi_user" class="vi-user float-r"><a href="/">YOUNGCY</a></div>--> 
+    <div id="vi_sign" class="vi-sign "> 
+     <div class="float-r"> 
+      <a href="#" style="font-size:130%;">登陆</a> 
+      <i style="color:#FCEE21;font-weight: bold;padding:0 5px">/</i> 
+      <a href="#">注册</a> 
+     </div> 
+    </div> 
+    <div id="vc_logo" class="logo"> 
+     <div class="icon"> 
+      <div class="logo-a"></div> 
+     </div> 
+    </div> 
+    <div id="vi_menu" class="vi-menu width-100p"> 
+     <ul> 
+      <li><a href="#">发现</a></li> 
+      <li class="active"><a href="#">动态</a> </li> 
+      <li><a href="#">作品</a></li> 
+      <li><a href="#">文章</a></li> 
+     </ul> 
+    </div> 
+    <div id="vi_main" class="width-100p "> 
+     <div class="main width-100p"> 
+      <ul> 
+       <li> 
+        <div class="activity"> 
+         <div class="activity-box"></div> 
+        </div> </li> 
+       <li> 
+        <div class="activity"> 
+         <div class="activity-box"></div> 
+        </div> </li> 
+       <li> 
+        <div class="activity"> 
+         <div class="activity-box"></div> 
+        </div> </li> 
+       <li> 
+        <div class="activity"> 
+         <div class="activity-box"></div> 
+        </div> </li> 
+       <li> 
+        <div class="activity"> 
+         <div class="activity-box"></div> 
+        </div> </li> 
+       <li> 
+        <div class="activity"> 
+         <div class="activity-box"></div> 
+        </div> </li> 
+      </ul> 
+      <div class="loadmore width-100p"> 
+       <a href="#" id="loadmore" class="btn load_btn"> 加载更多 </a> 
+      </div> 
+     </div> 
+    </div> 
+    <div id="vi_footer" class="width-100p"> 
+     <div class="vi_footer"> 
+      <div class="vi_footer_left"> 
+       <div>
+         &copy;&nbsp;artvc.cc&nbsp;京ICP备09025489号-4&nbsp; 
+        <a href="#">用户协议</a>&nbsp;-&nbsp;
+        <a href="#">隐私政策</a>&nbsp;-&nbsp;
+        <a href="#">联系我们</a>&nbsp;-&nbsp;
+        <a href="#">意见反馈</a> 
+       </div> 
+       <div class="float-l"> 
+        <a href="#"><i class="fa fa-weibo fa-2x icon"></i></a> 
+        <a href="#"><i class="fa fa-wechat fa-2x icon"></i></a> 
+        <a href="#"><i class="fa fa-facebook-official fa-2x icon"></i></a> 
+       </div> 
+      </div> 
+      <div class="vi_footer_right"> 
+       <img src="img/QRCode.png" /> 
+      </div> 
+     </div> 
+    </div> 
+   </div> 
+  </div>  
+  <script type="text/javascript" src="js/vchome.js"></script> 
+ </body>
 </html>
