e307c8c78f6e9b728072899693825ab23ddefc2a
JiaZhuo Chen <cjzishades@gmail.com>
2015-07-18 21:47:07 +0800

    7-18

--- application/controllers/article/Main.php
+++ application/controllers/article/Main.php
@@ -23,16 +23,14 @@ class Main extends MY_Controller {
         //获得页数
         $page = $this->sc->input('page');
         $uid  = isset($this->user['id']) ? $this->user['id'] : -1;
+
         $article = $this->article_service->get_article_list($page,$uid,$type);
-        if( ! empty($article))
-        {
-            //var_dump($article);
-            $this->load->view('main');
-        }
-        else
-        {
-            echo "failed";
-        }
-        
+
+        echo json_encode($article);
+    }
+
+    public function index()
+    {
+        $this->load->view('main');
     }
 }
