7e5295180d8824c5fb6fb007fb8290848005afcc
LiYOUNGCY <li.youngcy@gmail.com>
2015-07-24 20:29:30 +0800

    7-24

---
+++ application/views/active.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	<meta name="format-detection" content="telephone=yes" />
+	<meta name="msapplication-tap-highlight" content="no" />
+	<script type="text/javascript" src="<?=base_url().'public/'?>js/j162.min.js"></script>
+	<link href="<?=base_url().'public/'?>css/common.css" type="text/css" rel="stylesheet" />
+	<link href="<?=base_url().'public/'?>css/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
+	<input type="hidden" value="<?=base_url()?>" id="BASE_URL"> 
+</head>
+<body>
+	<div id="vc_sidebar" class="sidebar">
+		<div class="name">
+			<div class="head">
+				<a class="link" href="#">
+					<img src="<?=base_url().'public/'?>img/mm1.jpg" /></a>
+			</div>
+			<div class="text">
+				<a class="link" href="#">YOUNGCY</a>
+				<div class="identity">
+					<span class="icon identity"></span>
+				</div>
+			</div>
+		</div>
+		<div class="search">
+			<form>
+				<input id="" name="" type="text" />
+				<div>
+					<a class="link" href="#"> <i class="fa fa-search"></i>
+					</a>
+				</div>				
+			</form>
+
+		</div>
+		<div class="menu">
+			<ul>
+				<li class="menu-list">
+					<div class="menu-list-item">
+						<a class="link" href="<?=base_url()?>article/main">
+							<div class="icon">
+								<div class="home"></div>
+							</div>
+							<span class="menu-list-item-text">首页</span>
+						</a>
+					</div>
+				</li>
+				<a class="link" href="asasdas">
+					<li class="menu-list active">
+						<div class="menu-list-item">
+							<div class="icon">
+								<div class="guanzhu"></div>
+							</div>
+							<span class="menu-list-item-text">我的关注</span>
+						</div>
+					</li>
+				</a>
+				<li class="menu-list">
+					<div class="menu-list-item">
+						<a class="link" href="#">
+							<div class="icon">
+								<div class="tongji"></div>
+							</div>
+							<span class="menu-list-item-text">统计</span>
+						</a>
+					</div>
+				</li>
+				<li class="menu-list">
+					<div class="menu-list-item">
+						<a class="link" href="#">
+							<div class="icon">
+								<div class="sixin"></div>
+							</div>
+							<span class="menu-list-item-text">私信</span>
+						</a>
+					</div>
+				</li>
+				<li class="menu-list">
+					<div class="menu-list-item">
+						<a class="link" href="#">
+							<div class="icon">
+								<div class="setting"></div>
+							</div>
+							<span class="menu-list-item-text">设置</span>
+						</a>
+					</div>
+				</li>
+				<li class="menu-list">
+					<div class="menu-list-item">
+						<a class="link" href="#">
+							<div class="icon">
+								<div class="logout"></div>
+							</div>
+							<span class="menu-list-item-text">退出</span>
+						</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="vi_container" class="container">
+		<div id="shade"></div>
+		<div id="sbtn" class="sbtn">
+			<div class="icon">
+				<div class="sidebtn"></div>
+			</div>
+		</div>
+		<div id="vi_content" class="content">
+		<div id="vc_logo" class="logo">
+			<div class="icon">
+				<div class="logo-a"></div>
+			</div>
+		</div>
+		<div id="vi_menu" class="vi-menu width-100p">
+			<ul>
+				<li>
+					<a href="#" class="link">发现</a>
+				</li>
+				<li class="active">
+					<a href="#" class="link">动态</a>
+				</li>
+				<li>
+					<a href="#" class="link">作品</a>
+				</li>
+				<li>
+					<a href="#" class="link">文章</a>
+				</li>
+			</ul>
+		</div>
+		<div id="vi_main" class="width-100p ">
+			<div class="main width-100p">
+								
+				<div class="ac-right">
+					<div class="box ac-user">
+						<div class="name">
+							<div class="head">
+								<a href="#">
+									<img src="<?=base_url().'public/'?>img/mm1.jpg" /></a>
+							<div class="identity">
+									<span class="icon identity"></span>
+								</div>
+							</div>
+							<div class="text">
+								<a class="link" href="#">YOUNG啥第第六第六六届CY</a>
+							</div>
+
+						</div>
+						<div class="public">
+							<a href="" class="link">
+								<div class="btn">
+									<i class="fa fa-file-text"></i>发布文章
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="ac-left">
+					<div class="ac-boxlist clearfix">
+						<div class="box">
+							<div class="boxtop">
+								<div class="name">
+									<div class="head">
+										<a href="#">
+											<img src="<?=base_url().'public/'?>img/mm1.jpg" /></a>
+									<div class="identity">
+											<span class="icon identity"></span>
+										</div>
+									</div>
+									<div class="text">
+										<a class="link" href="#">YOUNG啥第六届CY</a>赞了<a class="link" href="#">artvc官方账号</a>的文章
+									</div>
+								</div>
+							</div>
+							<div class="article">
+								<div class="ar_text">
+									<div class="title">
+										<a class="link" href="#">阿斯顿阿斯顿</a>
+									</div>
+									
+									<div class="con">
+										<p>首先让我产生兴趣的是姜绥吾将他的绘画和装置艺术归于当代艺术，这说明他比较介意时间在各种艺术流派的进程中所起的作用。“当代”一词对于绘画艺术而言如同一个大熔炉，已经消化的，未经消化的，各种流派的，或具有实验性质的，都可以成为燃料或者...</p>	
+									</div>
+								</div>
+								<div class="ar_pic">
+									<img src="<?=base_url().'public/'?>img/mm1.jpg">		
+								</div>
+							</div>
+							<div class="btn"></div>
+						</div>
+						<div class="box">
+							<div class="boxtop">
+								<div class="name">
+									<div class="head">
+										<a href="#">
+											<img src="<?=base_url().'public/'?>img/mm1.jpg" /></a>
+									<div class="identity">
+											<span class="icon identity"></span>
+										</div>
+									</div>
+									<div class="text">
+										<a class="link" href="#">YOUNG啥第六届CY</a>赞了<a class="link" href="#">artvc官方账号</a>的文章
+									</div>
+								</div>
+							</div>
+							<div class="article">
+								<div class="ar_text">
+									<div class="title">
+										<a class="link" href="#">阿斯顿阿斯顿</a>
+									</div>
+									
+									<div class="con">
+										<p>首先让我产生兴趣的是姜绥吾将他的绘画和装置艺术归于当代艺术，这说明他比较介意时间在各种艺术流派的进程中所起的作用。“当代”一词对于绘画艺术而言如同一个大熔炉，已经消化的，未经消化的，各种流派的，或具有实验性质的，都可以成为燃料或者...</p>	
+									</div>
+								</div>
+								<div class="ar_pic">
+									<img src="<?=base_url().'public/'?>img/mm1.jpg">		
+								</div>
+							</div>
+							<div class="btn"></div>
+						</div>
+						<div class="box">
+							<div class="boxtop">
+								<div class="name">
+									<div class="head">
+										<a href="#">
+											<img src="<?=base_url().'public/'?>img/mm1.jpg" /></a>
+									<div class="identity">
+											<span class="icon identity"></span>
+										</div>
+									</div>
+									<div class="text">
+										<a class="link" href="#">YOUNG啥第六届CY</a>赞了<a class="link" href="#">artvc官方账号</a>的文章
+									</div>
+								</div>
+							</div>
+							<div class="article">
+								<div class="ar_text">
+									<div class="title">
+										<a class="link" href="#">阿斯顿阿斯顿</a>
+									</div>
+									
+									<div class="con">
+										<p>首先让我产生兴趣的是姜绥吾将他的绘画和装置艺术归于当代艺术，这说明他比较介意时间在各种艺术流派的进程中所起的作用。“当代”一词对于绘画艺术而言如同一个大熔炉，已经消化的，未经消化的，各种流派的，或具有实验性质的，都可以成为燃料或者...</p>	
+									</div>
+								</div>
+								<div class="ar_pic">
+									<img src="<?=base_url().'public/'?>img/mm1.jpg">		
+								</div>
+							</div>
+							<div class="btn"></div>
+						</div>
+						<div class="box">
+							<div class="boxtop">
+								<div class="name">
+									<div class="head">
+										<a href="#">
+											<img src="<?=base_url().'public/'?>img/mm1.jpg" /></a>
+									<div class="identity">
+											<span class="icon identity"></span>
+										</div>
+									</div>
+									<div class="text">
+										<a class="link" href="#">YOUNG啥第六届CY</a>赞了<a class="link" href="#">artvc官方账号</a>的文章
+									</div>
+								</div>
+							</div>
+							<div class="article">
+								<div class="ar_text">
+									<div class="title">
+										<a class="link" href="#">阿斯顿阿斯顿</a>
+									</div>
+									
+									<div class="con">
+										<p>首先让我产生兴趣的是姜绥吾将他的绘画和装置艺术归于当代艺术，这说明他比较介意时间在各种艺术流派的进程中所起的作用。“当代”一词对于绘画艺术而言如同一个大熔炉，已经消化的，未经消化的，各种流派的，或具有实验性质的，都可以成为燃料或者...</p>	
+									</div>
+								</div>
+								<div class="ar_pic">
+									<img src="<?=base_url().'public/'?>img/mm1.jpg">		
+								</div>
+							</div>
+							<div class="btn"></div>
+						</div>
+						<div class="loadmore width-100p">
+							<div id="loadmore" class="btn load_btn">
+								<font id="text">加载更多</font>
+								<i class="fa fa-spinner fa-pulse" style="text-decoration: none;display:none" id="icon"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			 </div>
+		 </div>
+			<div id="vi_footer" class="width-100p">
+				<div class="vi_footer">
+					<div class="vi_footer_left">
+						<div>
+							&copy;&nbsp;artvc.cc&nbsp;京ICP备09025489号-4&nbsp;
+							<a href="#" class="link">用户协议</a>
+							&nbsp;-&nbsp;
+							<a href="#" class="link">隐私政策</a>
+							&nbsp;-&nbsp;
+							<a href="#" class="link">联系我们</a>
+							&nbsp;-&nbsp;
+							<a href="#" class="link">意见反馈</a>
+						</div>
+						<div class="float-l">
+							<a href="#" class="link">
+								<i class="fa fa-weibo fa-2x icon"></i>
+							</a>
+							<a href="#" class="link">
+								<i class="fa fa-wechat fa-2x icon"></i>
+							</a>
+							<a href="#" class="link">
+								<i class="fa fa-facebook-official fa-2x icon"></i>
+							</a>
+						</div>
+					</div>
+					<div class="vi_footer_right">
+						<img src="<?=base_url().'public/'?>img/QRCode.png" /></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="<?=base_url().'public/'?>js/vchome.js"></script>
+</body>
+<script type="text/javascript">
+
+	window.onload = function() { 
+
+	}; 
+
+</script>
+</html>
