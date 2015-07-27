f3dc5d9986fa2c325554e4373a6dc66dfd7bace2
LiYOUNGCY <li.youngcy@gmail.com>
2015-07-27 14:46:32 +0800

    7-26

--- application/views/feed.php
+++ application/views/feed.php
@@ -189,25 +189,25 @@
         data = eval("("+data+")"); 
 				for(var i = 0; i < data.length; i++)  
 				{
-					var id 										= data[i].id;
-					var feed_type 						= data[i].type;
-					var article 							= eval("(" +data[i].content+ ")");
-					var article_id						= article.article_id;
-					var article_title					= article.article_title;
-					var article_content				= article.article_content;
-					var user_head_src					= data[i].user.pic;
-					var user_name							= data[i].user.name;
-					var user_alias						= data[i].user.alias;
-					var like									= data[i].like;
-					var like_num							= like.length;
-					var time 									= data[i].publish_time;
-					var element 							=	"";
+					var id 					= data[i].id;
+					var feed_type 			= data[i].type;
+					var article 			= eval("(" +data[i].content+ ")");
+					var article_id			= article.article_id;
+					var article_title		= article.article_title;
+					var article_content		= article.article_content;
+					var user_head_src		= data[i].user.pic;
+					var user_name			= data[i].user.name;
+					var user_alias			= data[i].user.alias;
+					var like				= data[i].like;
+					var like_num			= like.length;
+					var time 				= data[i].publish_time;
+					var element 			=	"";
 					if(feed_type == 1){
-						var author							= data[i].author.name;
-						var author_alias				= data[i].author.alias;	
-						var action							= '<a class="link" href="#">'+ user_name +'</a>赞了<a class="link" href="#">'+ author +'</a>的一篇文章';
+						var author			= data[i].author.name;
+						var author_alias	= data[i].author.alias;	
+						var action			= '<a class="link" href="#">'+ user_name +'</a>赞了<a class="link" href="#">'+ author +'</a>的一篇文章';
 					}else{
-						var action							= '<a class="link" href="#">'+ user_name +'</a>发布了一篇文章';
+						var action			= '<a class="link" href="#">'+ user_name +'</a>发布了一篇文章';
 					}
 
 					element = '<div class="box"><div class="boxtop"><div class="name"><div class="head"><a href="#"><img src="'+ user_head_src +'" /></a><div class="identity"><span class="icon identity"></span></div></div><div class="text">'+ action +'</div></div><div class="time"><time class="timeago" title="'+ time +'" datetime="'+ time +'+08:00"></time></div></div><div class="article"><div class="ar_text"><div class="title"><a class="link" href="'+ BASE_URL +'article/'+article_id+'">'+ article_title +'</a></div><div class="con"><p>'+ article_content +'</p></div></div><div class="ar_pic"><a href="'+ BASE_URL +'article/'+article_id+'"><img  id="'+ id +'"></a></div></div><div class="support"><div class="like float-l"><div class="btn"><i class="fa fa-heart"></i> '+ like_num +'</div></div><div class="list">';
@@ -219,18 +219,18 @@
 					$(".timeago").timeago();
 				}
       }
-		});
+	});
 
-		$.ajax({
-      url: GET_FEED_LIST_URL,
-      type: 'POST',
-      data:{page : pageTemp},
-      success: function(data) {
+	$.ajax({
+        url: GET_FEED_LIST_URL,
+        type: 'POST',
+        data:{page : pageTemp},
+        success: function(data) {
           data = eval("("+data+")"); 
 				for(var i = 0; i < data.length; i++)  
 				{  
 					var article 	= eval("(" +data[i].content+ ")");
-					var id 				= data[i].id;
+					var id 			= data[i].id;
 					var image 		= article.article_image;
 					$(".ar_pic #"+id).attr("src",image);
 				}
