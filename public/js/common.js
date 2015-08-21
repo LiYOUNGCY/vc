a74cb23ee0b5a55ca1755bdc508c98f6ede12b83
han <897475686@qq.com>
2015-08-22 00:57:24 +0800

    '8-22'

---
+++ public/js/common.js
@@ -0,0 +1,35 @@
+
+var BASE_URL = document.getElementById('BASE_URL').value;
+var GET_ARTICLE_URL = BASE_URL + "article/main/get_article_list";
+var COMMENT_URL = BASE_URL + 'article/detail/write_comment';
+var VOTE_URL = BASE_URL + 'article/detail/get_vote_list';
+var ARGEE_URL = BASE_URL + 'article/detail/vote_article';
+var DELETE_URL = BASE_URL + 'article/detail/delete_article';
+
+//艺术家列表
+var GET_ARTIST_LIST = BASE_URL + 'artist/main/get_artist_list';
+
+//登录
+var LOGIN_URL = BASE_URL + 'login';
+
+
+
+
+//艺术品列表
+
+var PHONE_LOGIN_URL = BASE_URL + "account/main/login_by_phone";
+var EMAIL_LOGIN_URL = BASE_URL + "account/main/login_by_email";
+var EMAIL_SIGNUP_URL = BASE_URL + "account/main/register_by_email";
+var PHONE_SIGNUP_URL = BASE_URL + "account/main/register_by_phone";
+var CHECK_PHONE_URL = BASE_URL + "account/main/check_phone";
+var CHECK_EMAIL_URL = BASE_URL + "account/main/check_email";   
+
+var GET_PRODUCTION_URL = BASE_URL + 'production/main/get_production_list';
+
+/**
+ * [trim 去掉字符串前后两端的空格]
+ */
+function trim(str)
+{
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
