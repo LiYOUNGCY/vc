6a6ec9a6d928619c77c103002facf9d4aafb8af8
JiaZhuo Chen <jiazhuochen.web@gmail.com>
2015-10-02 22:53:26 +0800

    10-2

--- application/views/common/head.php
+++ application/views/common/head.php
@@ -13,8 +13,6 @@
     <title>Artvc - <?= $title ?></title>
 
     <!-- 导入 Javascript -->
-    <script src="<?= base_url() ?>public/js/jquery.js"></script>
-
     <script src="<?= base_url() ?>public/js/validate.js"></script>
     <?php
     if (isset($javascript)) {
