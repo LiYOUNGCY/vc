11677b6695efee7275f027b9e5a3c08e630f3afa
Rache <jiazhuochen.web@gmail.com>
2015-10-28 13:31:52 +0800

    10-28

--- application/controllers/pay/Main.php
+++ application/controllers/pay/Main.php
@@ -15,83 +15,53 @@ class Main extends MY_Controller
      */
     public function index()
     {
-        require_once APPPATH . 'third_party/alipay/alipay.config.php';
-        require_once APPPATH . 'third_party/alipay/lib/alipay_submit.class.php';
-
-//        var_dump($alipay_config);
+        require_once(APPPATH . 'third_party/alipay/alipay.config.php');
+        require_once(APPPATH . 'third_party/alipay/lib/alipay_submit.class.php');
 
         /**************************请求参数**************************/
 
         //支付类型
-        $payment_type = '1';
+        $payment_type = "1";
         //必填，不能修改
         //服务器异步通知页面路径
-        $notify_url = 'http://www.artvc.cc/pay/success';
         //需http://格式的完整路径，不能加?id=123这类自定义参数
 
         //页面跳转同步通知页面路径
-        $return_url = 'http://www.artvc.cc/pay/success';
+        $return_url = "http://localhost/artvc/pay/success";
         //需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
 
         //商户订单号
-        // $out_trade_no = $_POST['WIDout_trade_no'];
+//        $out_trade_no = $_POST['WIDout_trade_no'];
         $out_trade_no = time();
         //商户网站订单系统中唯一订单号，必填
 
         //订单名称
-        // $subject = $_POST['WIDsubject'];
         $subject = 'TEST';
         //必填
 
         //付款金额
-        // $total_fee = $_POST['WIDtotal_fee'];
-        $total_fee = 1;
+        $total_fee = 0.1;
         //必填
-
-        //订单描述
-
-        // $body = $_POST['WIDbody'];
-        $body = 'TEST';
-        //商品展示地址
-        // $show_url = $_POST['WIDshow_url'];
-        $show_url = 'www.artvc.cc';
-        //需以http://开头的完整路径，例如：http://www.商户网址.com/myorder.html
-
-        //防钓鱼时间戳
-        $anti_phishing_key = '';
-        //若要使用请调用类文件submit中的query_timestamp函数
-
-        //客户端的IP地址
-        $exter_invoke_ip = '';
-        //非局域网的外网IP地址，如：221.0.0.1
 
 
         /************************************************************/
 
-        //构造要请求的参数数组，无需改动
+//构造要请求的参数数组，无需改动
         $parameter = array(
-            'service' => 'create_direct_pay_by_user',
-            'partner' => trim($alipay_config['partner']),
-            'seller_id' => trim($alipay_config['partner']),
-            'seller_email' => trim($alipay_config['seller_email']),
-            'payment_type' => $payment_type,
-            'notify_url' => $notify_url,
-            'return_url' => $return_url,
-            'out_trade_no' => $out_trade_no,
-            'subject' => $subject,
-            'total_fee' => $total_fee,
-            'body' => $body,
-            'show_url' => $show_url,
-            'anti_phishing_key' => $anti_phishing_key,
-            'exter_invoke_ip' => $exter_invoke_ip,
-            '_input_charset' => trim(strtolower($alipay_config['input_charset'])),
+            "service" => "create_direct_pay_by_user",
+            "partner" => trim($alipay_config['partner']),
+            "seller_email" => trim($alipay_config['seller_email']),
+            "payment_type" => $payment_type,
+            "return_url" => $return_url,
+            "out_trade_no" => $out_trade_no,
+            "subject" => $subject,
+            "total_fee" => $total_fee,
+            "_input_charset" => trim(strtolower($alipay_config['input_charset']))
         );
 
-        //建立请求
+//建立请求
         $alipaySubmit = new AlipaySubmit($alipay_config);
-//        var_dump($alipaySubmit);
-        $html_text = $alipaySubmit->buildRequestForm($parameter, 'get', '确认');
-//        var_dump($html_text);
+        $html_text = $alipaySubmit->buildRequestForm($parameter, "get", "确认");
         echo $html_text;
     }
 
@@ -100,13 +70,12 @@ class Main extends MY_Controller
      */
     public function success()
     {
-        require_once APPPATH . 'third_party/alipay/alipay.config.php';
-        require_once APPPATH . 'third_party/alipay/lib/alipay_notify.class.php';
-        //计算得出通知验证结果
+        require_once(APPPATH . 'third_party/alipay/alipay.config.php');
+        require_once(APPPATH . 'third_party/alipay/lib/alipay_notify.class.php');
+//计算得出通知验证结果
         $alipayNotify = new AlipayNotify($alipay_config);
         $verify_result = $alipayNotify->verifyReturn();
-        if ($verify_result) {
-            //验证成功
+        if ($verify_result) {//验证成功
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             //请在这里加上商户的业务逻辑程序代码
 
@@ -114,23 +83,27 @@ class Main extends MY_Controller
             //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表
 
             //商户订单号
+
             $out_trade_no = $_GET['out_trade_no'];
 
             //支付宝交易号
+
             $trade_no = $_GET['trade_no'];
 
             //交易状态
             $trade_status = $_GET['trade_status'];
 
+
             if ($_GET['trade_status'] == 'TRADE_FINISHED' || $_GET['trade_status'] == 'TRADE_SUCCESS') {
                 //判断该笔订单是否在商户网站中已经做过处理
                 //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
                 //如果有做过处理，不执行商户的业务程序
+                echo '恭喜你，支付成功';
             } else {
-                echo 'trade_status=' . $_GET['trade_status'];
+                echo "trade_status=" . $_GET['trade_status'];
             }
 
-            echo '验证成功<br />';
+            echo "验证成功<br />";
 
             //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
 
@@ -138,7 +111,7 @@ class Main extends MY_Controller
         } else {
             //验证失败
             //如要调试，请看alipay_notify.php页面的verifyReturn函数
-            echo '验证失败';
+            echo "验证失败";
         }
     }
 }
--- application/third_party/alipay/lib/alipay_core.function.php
+++ application/third_party/alipay/lib/alipay_core.function.php
@@ -10,16 +10,15 @@
  */
 
 /**
- * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串.
- *
+ * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
  * @param $para 需要拼接的数组
  * return 拼接完成以后的字符串
  */
 function createLinkstring($para)
 {
-    $arg = '';
-    while (list($key, $val) = each($para)) {
-        $arg .= $key.'='.$val.'&';
+    $arg = "";
+    while (list ($key, $val) = each($para)) {
+        $arg .= $key . "=" . $val . "&";
     }
     //去掉最后一个&字符
     $arg = substr($arg, 0, count($arg) - 2);
@@ -31,17 +30,17 @@ function createLinkstring($para)
 
     return $arg;
 }
+
 /**
- * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串，并对字符串做urlencode编码.
- *
+ * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串，并对字符串做urlencode编码
  * @param $para 需要拼接的数组
  * return 拼接完成以后的字符串
  */
 function createLinkstringUrlencode($para)
 {
-    $arg = '';
-    while (list($key, $val) = each($para)) {
-        $arg .= $key.'='.urlencode($val).'&';
+    $arg = "";
+    while (list ($key, $val) = each($para)) {
+        $arg .= $key . "=" . urlencode($val) . "&";
     }
     //去掉最后一个&字符
     $arg = substr($arg, 0, count($arg) - 2);
@@ -53,28 +52,28 @@ function createLinkstringUrlencode($para)
 
     return $arg;
 }
+
 /**
- * 除去数组中的空值和签名参数.
- *
+ * 除去数组中的空值和签名参数
  * @param $para 签名参数组
  * return 去掉空值与签名参数后的新签名参数组
  */
 function paraFilter($para)
 {
+    reset($para);
     $para_filter = array();
-    while (list($key, $val) = each($para)) {
-        if ($key == 'sign' || $key == 'sign_type' || $val == '') {
+    while (list ($key, $val) = each($para)) {
+        if ($key == "sign" || $key == "sign_type" || $val == "") {
             continue;
         } else {
             $para_filter[$key] = $para[$key];
         }
     }
-
     return $para_filter;
 }
+
 /**
- * 对数组排序.
- *
+ * 对数组排序
  * @param $para 排序前的数组
  * return 排序后的数组
  */
@@ -82,20 +81,19 @@ function argSort($para)
 {
     ksort($para);
     reset($para);
-
     return $para;
 }
+
 /**
  * 写日志，方便测试（看网站需求，也可以改成把记录存入数据库）
- * 注意：服务器需要开通fopen配置.
- *
+ * 注意：服务器需要开通fopen配置
  * @param $word 要写入日志里的文本内容 默认值：空值
  */
 function logResult($word = '')
 {
-    $fp = fopen('log.txt', 'a');
+    $fp = fopen("log.txt", "a");
     flock($fp, LOCK_EX);
-    fwrite($fp, '执行日期：'.strftime('%Y%m%d%H%M%S', time())."\n".$word."\n");
+    fwrite($fp, "执行日期：" . strftime("%Y%m%d%H%M%S", time()) . "\n" . $word . "\n");
     flock($fp, LOCK_UN);
     fclose($fp);
 }
@@ -104,8 +102,7 @@ function logResult($word = '')
  * 远程获取数据，POST模式
  * 注意：
  * 1.使用Crul需要修改服务器中php.ini文件的设置，找到php_curl.dll去掉前面的";"就行了
- * 2.文件夹中cacert.pem是SSL证书请保证其路径有效，目前默认路径是：getcwd().'\\cacert.pem'.
- *
+ * 2.文件夹中cacert.pem是SSL证书请保证其路径有效，目前默认路径是：getcwd().'\\cacert.pem'
  * @param $url 指定URL完整路径地址
  * @param $cacert_url 指定当前工作目录绝对路径
  * @param $para 请求的数据
@@ -114,8 +111,9 @@ function logResult($word = '')
  */
 function getHttpResponsePOST($url, $cacert_url, $para, $input_charset = '')
 {
+
     if (trim($input_charset) != '') {
-        $url = $url.'_input_charset='.$input_charset;
+        $url = $url . "_input_charset=" . $input_charset;
     }
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);//SSL证书认证
@@ -136,8 +134,7 @@ function getHttpResponsePOST($url, $cacert_url, $para, $input_charset = '')
  * 远程获取数据，GET模式
  * 注意：
  * 1.使用Crul需要修改服务器中php.ini文件的设置，找到php_curl.dll去掉前面的";"就行了
- * 2.文件夹中cacert.pem是SSL证书请保证其路径有效，目前默认路径是：getcwd().'\\cacert.pem'.
- *
+ * 2.文件夹中cacert.pem是SSL证书请保证其路径有效，目前默认路径是：getcwd().'\\cacert.pem'
  * @param $url 指定URL完整路径地址
  * @param $cacert_url 指定当前工作目录绝对路径
  * return 远程输出的数据
@@ -158,8 +155,7 @@ function getHttpResponseGET($url, $cacert_url)
 }
 
 /**
- * 实现多种字符编码方式.
- *
+ * 实现多种字符编码方式
  * @param $input 需要编码的字符串
  * @param $_output_charset 输出的编码格式
  * @param $_input_charset 输入的编码格式
@@ -167,25 +163,20 @@ function getHttpResponseGET($url, $cacert_url)
  */
 function charsetEncode($input, $_output_charset, $_input_charset)
 {
-    $output = '';
-    if (!isset($_output_charset)) {
-        $_output_charset = $_input_charset;
-    }
+    $output = "";
+    if (!isset($_output_charset)) $_output_charset = $_input_charset;
     if ($_input_charset == $_output_charset || $input == null) {
         $output = $input;
-    } elseif (function_exists('mb_convert_encoding')) {
+    } elseif (function_exists("mb_convert_encoding")) {
         $output = mb_convert_encoding($input, $_output_charset, $_input_charset);
-    } elseif (function_exists('iconv')) {
+    } elseif (function_exists("iconv")) {
         $output = iconv($_input_charset, $_output_charset, $input);
-    } else {
-        die('sorry, you have no libs support for charset change.');
-    }
-
+    } else die("sorry, you have no libs support for charset change.");
     return $output;
 }
+
 /**
- * 实现多种字符解码方式.
- *
+ * 实现多种字符解码方式
  * @param $input 需要解码的字符串
  * @param $_output_charset 输出的解码格式
  * @param $_input_charset 输入的解码格式
@@ -193,19 +184,16 @@ function charsetEncode($input, $_output_charset, $_input_charset)
  */
 function charsetDecode($input, $_input_charset, $_output_charset)
 {
-    $output = '';
-    if (!isset($_input_charset)) {
-        $_input_charset = $_input_charset;
-    }
+    $output = "";
+    if (!isset($_input_charset)) $_input_charset = $_input_charset;
     if ($_input_charset == $_output_charset || $input == null) {
         $output = $input;
-    } elseif (function_exists('mb_convert_encoding')) {
+    } elseif (function_exists("mb_convert_encoding")) {
         $output = mb_convert_encoding($input, $_output_charset, $_input_charset);
-    } elseif (function_exists('iconv')) {
+    } elseif (function_exists("iconv")) {
         $output = iconv($_input_charset, $_output_charset, $input);
-    } else {
-        die('sorry, you have no libs support for charset changes.');
-    }
-
+    } else die("sorry, you have no libs support for charset changes.");
     return $output;
 }
+
+?>
